6d7528e9c894555674b45c11d24820a81c2a098d
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-03-06 12:14:56 +0530

    done

--- db/db.js
+++ db/db.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const USERNAME  = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD
 
+
+
 const MongoConnected  = ()=>{
 
     const MONGODB_URL = `mongodb://${USERNAME}:${PASSWORD}@ac-dyxtfcd-shard-00-00.mbiyul4.mongodb.net:27017,ac-dyxtfcd-shard-00-01.mbiyul4.mongodb.net:27017,ac-dyxtfcd-shard-00-02.mbiyul4.mongodb.net:27017/?ssl=true&replicaSet=atlas-sixufj-shard-0&authSource=admin&retryWrites=true&w=majority`;
